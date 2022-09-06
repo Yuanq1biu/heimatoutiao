3569eba45aa9cd4d8ca1533b9fe03b6a8d4a8a83
yuanqi <815180488@qq.com>
2022-09-06 16:13:29 +0800

    postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="test"></div>
+  </div>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style scoped>
+.test{
+  width: 2rem;
+  height: 3rem;
+  background-color: pink;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// css
+import '@/assets/fonts/iconfont.css'
+// vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// rem适配
+import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
